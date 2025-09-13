03b67bd110f09bb549502d07df41820a64d2c44e
Kartik Kujala <kartikkujala@Kartiks-MacBook-Air.local>
2025-09-13 17:08:37 -0500

    init

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navbar from './navbar';
 
 function Hero() {
   return (
-    <div>
+    <nav className="flex justify-center items-center py-16 px-8 border-b bg-blue-100 border-gray-300 text-black">
+      <div className="flex gap-2">
+                
+        <span className="text-4xl font-medium text-gray-900">Maxing our</span>
+        <span className="text-4xl font-extrabold text-gray-900">GPA</span>
+        <p className="text-4xl font-medium text-gray-900">One</p>
+        <p className="text-4xl font-extrabold text-gray-900">Class</p>
+        <p className="text-4xl font-medium text-gray-900">at a time</p>
 
-    </div>
+                
+      </div>
+    </nav>
   );
 }
 
